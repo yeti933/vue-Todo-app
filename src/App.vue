14490037ef525276ff0f605cea4f06365aa47e91
Bogdan Ostojic <bogdanostojic1993@gmail.com>
2019-06-29 16:11:53 +0200

    Todo App

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,27 @@
 </template>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+ * {
+   box-sizing: border-box;
+   margin: 0;
+   padding: 0;
+ }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+ body {
+   font-family:  Arial, Helvetica, sans-serif;
+   line-height: 1.4;
+ }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+ .btn {
+   display: inline-block;
+   border: none;
+   background: #555;
+   color: #fff;
+   padding: 7px 20px;
+   cursor: pointer;
+ }
+
+ .btn:hover {
+   background: #666;
+ }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "../components/layout/Header"
+import Todos from "../components/Todos"
+import AddTodo from "../components/AddTodo";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      console.log(id)
+      const url =`https://jsonplaceholder.typicode.com/todos/${id}`;
+      const postParam = {
+        method: 'DELETE'
+      };
+
+      fetch(url, postParam)
+        .then( res => this.todos = this.todos.filter( todo => todo.id !== id))
+        .catch( err => console.log(err));
+    },
+    addTodo(Todo) {
+      const { title, completed} = Todo;
+      const url =`https://jsonplaceholder.typicode.com/todos`;
+      const postParam = {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({title, completed})
+      };
+
+      fetch(url, postParam)
+        .then( res => res.json())
+        .then( res => this.todos = [...this.todos, res])
+        .catch( err => console.log(err));
+    }
+  },
+  created() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10 ')
+      .then( res => res.json())
+      .then( resJson => this.todos = resJson);
   }
 };
 </script>
